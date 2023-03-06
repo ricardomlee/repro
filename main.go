307ff35cea37f7b2c808f5e0aece0d574043e977
Ric Li <ricardomlee@gmail.com>
2023-03-06 22:34:21 +0800

    add https feature (#2)
    * add https feature
    
    * update go mod

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 
 	"github.com/BurntSushi/toml"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 type Config struct {
 	Proxies map[string]string
+	Cert    struct{ Dir string }
 }
 
 func main() {
 	// 读取配置文件
 	config := Config{}
-	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
+	if _, err := toml.DecodeFile("/config/repro.toml", &config); err != nil {
 		log.Fatal(err)
 	}
+
+	var domains []string
 
 	// 创建反向代理实例
 	reverseProxies := make(map[string]*httputil.ReverseProxy)
@@ -28,10 +33,13 @@ func main() {
 			log.Fatalf("Invalid target URL for %s: %s", name, err)
 		}
 		reverseProxies[name] = httputil.NewSingleHostReverseProxy(target)
+		domains = append(domains, name)
+		log.Println("Add proxy for", name)
 	}
 
 	// 创建HTTP服务器并注册反向代理处理器
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		name := r.Host // 使用请求的域名作为反向代理的名称
 		if proxy, ok := reverseProxies[name]; ok {
 			proxy.ServeHTTP(w, r)
@@ -40,9 +48,53 @@ func main() {
 		}
 	})
 
-	// 启动HTTP服务器
-	log.Println("Starting reverse proxy server on :80")
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		log.Fatal(err)
+	if config.Cert.Dir != "" { // 使用HTTPS
+		log.Println("Using cert cache dir", config.Cert.Dir)
+		// 设置证书管理器
+		m := autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			Cache:      autocert.DirCache(config.Cert.Dir),
+			HostPolicy: autocert.HostWhitelist(domains...),
+		}
+
+		// 创建HTTPS服务器
+		tlsConfig := &tls.Config{
+			GetCertificate: m.GetCertificate,
+		}
+		server := &http.Server{
+			Addr:      ":https",
+			Handler:   mux,
+			TLSConfig: tlsConfig,
+		}
+
+		// 启动HTTPS服务器
+		go func() {
+			log.Println("Starting HTTPS server on :https")
+			if err := server.ListenAndServeTLS("", ""); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		// 创建HTTP服务器并重定向到HTTPS服务器
+		httpServer := &http.Server{
+			Addr:      ":http",
+			Handler:   m.HTTPHandler(nil),
+			TLSConfig: tlsConfig,
+		}
+		log.Println("Starting HTTP server on :http")
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	} else { // 使用HTTP
+		// 创建HTTP服务器
+		httpServer := &http.Server{
+			Addr:    ":http",
+			Handler: mux,
+		}
+		log.Println("Starting HTTP server on :http")
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
 	}
+
 }
